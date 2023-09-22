a0d2bac59b7ee7ff3c74ad3d20ff2f01129b00ef
349078 <ShaneSmith@fnbo.com>
2023-09-22 11:33:08 -0400

    update for the morning

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,43 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
+import { initializeConnect } from "react-redux/es/components/connect";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initalState = { counter: 0, showCounter: true }
+
+createSlice({
+	name: 'counter',
+	initialState,
+	reducers: {
+		increment(state) {},
+		decrement() {},
+		increase() {},
+		toggleCounter() {},
+	}
+});
+const counterReducer = (state = initalState, action) => {
 	if (action.type === "increment") {
 		return {
 			counter: state.counter + 1,
+			showCounter: state.showCounter
 		};
+	}
+	if (action.type === "increase") {
+		return {
+			counter: state.counter + action.amount,
+			showCounter: state.showCounter
+		}
 	}
 	if (action.type === "decrement") {
 		return {
 			counter: state.counter - 1,
+			showCounter: state.showCounter
 		};
+	}
+	if (action.type === 'toggle') {
+		return {
+			counter: state.counter,
+			showCounter: !state.showCounter
+		}
 	}
 	return state;
 };
